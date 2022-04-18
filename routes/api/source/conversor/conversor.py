acfa786e21a933c7e66b1093c3ca762262eb54aa
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-04-18 20:04:36 -0300

    Reestructured source directory

---
+++ routes/api/source/conversor/conversor.py
@@ -0,0 +1,11 @@
+from .moviepy import AudioFileClip
+from pathlib import Path
+
+from ..interfaces import ConversorInterface
+
+class Conversor(ConversorInterface):
+    @classmethod
+    def convert(cls, audio : str) -> None:
+        clip = AudioFileClip(audio)
+        filename = audio.split("/")[-1].replace(".mp3", "")
+        clip.write_audiofile(f'{Path().absolute()}/static/{filename}.mp3')
---
+++ routes/api/source/hash/hash.py
@@ -0,0 +1,13 @@
+from hashlib import sha256
+from datetime import datetime
+from random import randint
+
+from ..interfaces import HashInterface
+
+class Hash(HashInterface) :
+    @classmethod
+    def generate_random_hash(cls) -> str :
+        encoder = 'utf-8'
+        time = str(datetime.now()).encode(encoder)
+        random = str(randint(0, 1000)).encode(encoder)
+        return sha256(f'{time}{random}'.encode(encoder)).hexdigest()[:8] 
