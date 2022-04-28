75cfe24f22c980f96936346aa042b7d3ce6b1dea
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-04-28 20:16:58 -0300

    Tests update, update the factory

--- tests/local/factory_test.py
+++ tests/local/factory_test.py
@@ -1,7 +1,11 @@
+from sys import path
+path.append('../../')
 from routes.api.source import Factory
 from routes.api.source.hash import Hash
 from routes.api.source.conversor import Conversor
 from routes.api.source.interfaces import FactoryInterface, HashInterface, ConversorInterface
+from routes.api.source.api_routes_class.interfaces import DeleteFilesRouteInterface
+from routes.api.source.api_routes_class import DeleteFilesRoute
 
 def test_answer() :
     fac = Factory()
@@ -9,9 +13,20 @@ def test_answer() :
 
     # Is instance 
 
-    assert isinstance(fac.get_representative(HashInterface), Hash)
-    assert isinstance(fac.get_representative(ConversorInterface), Conversor)
+    # assert isinstance(fac.get_representative(HashInterface), Hash)
+    # assert isinstance(fac.get_representative(ConversorInterface), Conversor)
 
     # Is not instance
 
-    assert not isinstance(fac.get_representative(ConversorInterface), Hash)
+    # assert not isinstance(fac.get_representative(ConversorInterface), Hash)
+
+    ## Delete Files Route
+
+    assert isinstance(fac.get_representative(DeleteFilesRouteInterface, '123'), DeleteFilesRoute)
+
+    # Is not instance
+    
+    assert not isinstance(fac.get_representative(DeleteFilesRouteInterface), Hash)
+
+if __name__ == '__main__' : 
+    test_answer()
