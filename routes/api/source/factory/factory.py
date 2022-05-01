65e1f266076978960b8a044971f0922b2e3c10e1
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-05-01 17:24:35 -0300

    Update factory

--- routes/api/source/factory/factory.py
+++ routes/api/source/factory/factory.py
@@ -8,17 +8,27 @@ from abc import ABC
 from ..interfaces import FactoryInterface
 from ..hash import Hash
 from ..conversor import Conversor
+from ..api_routes_class import DeleteFilesRoute
 
 class Factory(FactoryInterface) :
     def __init__(self) -> None:
         self.__representatives : Tuple[ABC] = (
             Hash,
-            Conversor
+            Conversor,
+
+            # Routes
+
+            DeleteFilesRoute
         )
 
-    def get_representative(self, interface: Type[ABC], *args, **kwargs) -> ABC:
+    def get_representative(self, interface: Type[ABC]) -> ABC:
+
         for representative in self.__representatives :
-            if isinstance(representative(*args, **kwargs), interface) :
-                return representative(*args, **kwargs)
+            try : 
+                if isinstance(representative(), interface) :
+                    return representative()
+            except TypeError as error:
+                print(error)
+
         raise ValueError(f'{interface} has no representative')
 
