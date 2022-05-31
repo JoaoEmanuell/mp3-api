74a9038b99e2d373a53f5077b476d42fd02f58dd
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-05-31 18:02:28 -0300

    GetConvertedAudioRoute and update GetConvertedAudioRouteInterface

--- routes/api/source/api_routes_class/interfaces/get_converted_audio_route_interface.py
+++ routes/api/source/api_routes_class/interfaces/get_converted_audio_route_interface.py
@@ -4,8 +4,39 @@ from typing import Dict
 class GetConvertedAudioRouteInterface(ABC):
     @abstractmethod
     def __init__(self, filename : str, url_base : str):
+        """Init
+
+        Args:
+            filename (str): Filename provide by route get parameter.
+            url_base (str): Url base provide by route, use request to access it.
+
+        """        
         raise NotImplementedError()
 
     @abstractmethod
     def main(self) -> Dict[str, str] :
+        """Main class
+
+        Raises:
+            NotImplementedError: _description_
+
+        Returns:
+            Dict[str, str]: Dict with two keys, 
+                audio : path to audio converted. 
+                filename : name of audio converted.
+        """        
+        raise NotImplementedError()
+
+    @abstractmethod
+    def set_atributes(self, **kwargs) :
+        """
+        Set atributes
+
+        Args:
+            **kwargs (dict): Dict of atributes
+            attributes are defined in __init__
+
+        Raises:
+            NotImplementedError: NotImplementedError
+        """       
         raise NotImplementedError()
