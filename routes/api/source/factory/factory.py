4199fd3c74b819d554775d6a54a5309c027b7045
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-09-17 11:10:01 -0300

    Update factory.py

--- routes/api/source/factory/factory.py
+++ routes/api/source/factory/factory.py
@@ -7,14 +7,16 @@ from abc import ABC
 
 from ..interfaces import FactoryInterface
 from ..hash import Hash
-from ..conversor import Conversor
+from ..conversor import ConversorFfmpeg
+from ..conversor.ffmpeg import ExtractLogInfos
 from ..api_routes_class import DeleteFilesRoute, UploadAudioRoute, GetConvertedAudioRoute, GetStatusFileRoute
 
 class Factory(FactoryInterface) :
     def __init__(self) -> None:
         self.__representatives : Tuple[ABC] = (
             Hash,
-            Conversor,
+            ConversorFfmpeg,
+            ExtractLogInfos,
 
             # Routes
 
