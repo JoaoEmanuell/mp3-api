b0bc4c82c190cf79a27771deac226bb1e8b1b6f3
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-09-17 10:35:03 -0300

    Extract log infos interface

--- routes/api/source/interfaces/__init__.py
+++ routes/api/source/interfaces/__init__.py
@@ -1,3 +1,4 @@
 from .conversor_interface import ConversorInterface
 from .hash_interface import HashInterface
 from .factory_interface import FactoryInterface
+from .extract_log_infos_interface import ExtractLogInfosInterface
---
+++ routes/api/source/interfaces/extract_log_infos_interface.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Type
+
+class ExtractLogInfosInterface(ABC):
+    """Extract data from ffmpeg conversion log"""
+
+    @abstractmethod
+    def __init__(self, log_path: str, error_class: Type[Exception]) -> None:
+        """Init
+
+        Args:
+            log_path (str): Path to ffmpeg conversion log
+            error_class (Type[Exception]): Error class to raise erros
+
+        """        
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_seconds(self) -> int:
+        """Get seconds of the audio
+
+        Returns:
+            int: duration in seconds of the audio
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_bitrate(self) -> int:
+        """Get bitrate
+
+        Returns:
+            int: bitrate in kilobits per second.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_current_file_size(self) -> Dict[str, int]:
+        """Get current file size
+
+        Raises:
+            ExtractLogInfosErrosInterface: 
+                "Conversion Error!" case conversion error.
+                "Log Error!" case the log is invalid or not data.
+
+        Returns:
+            Dict[str, int]: {message: size}
+        """        
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_estimated_file_size(self) -> int:
+        """Return the mp3 estimated file size
+
+        Returns:
+            int: size in kb
+        """        
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_filename(self) -> str:
+        """Get filename
+
+        Returns:
+            str: Filename from file
+        """        
+        raise NotImplementedError()
